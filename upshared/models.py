6543bb71f7788b16163bab01952bd79f1dc9c1fb
chencccc <602530268@qq.com>
2019-12-19 00:25:39 +0800

    edit

--- upshared/models.py
+++ upshared/models.py
@@ -5,8 +5,8 @@ from datetime import datetime
 class File(db.Model):
     __tablename__ = 'file'
     id = db.Column(db.Integer, primary_key=True)
-    create_time = db.Column(db.DateTime, default=datetime.now())
-    update_time = db.Column(db.DateTime, default=datetime.now())
+    create_time = db.Column(db.DateTime, default=datetime.now
+    update_time = db.Column(db.DateTime, default=datetime.now
 
     name = db.Column(db.String(50),unique=True) # 文件名
     type = db.Column(db.String(50))   # 后缀
